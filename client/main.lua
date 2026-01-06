2b97eb47bac763f3df8cdd6b515690b5eedb7338
PokjadAnaqi <alypheyjad1@gmail.com>
2026-01-07 04:10:42 +0800

    Update main.lua
    add ox_target support

--- client/main.lua
+++ client/main.lua
@@ -143,22 +143,59 @@ end
 
 
 --// Box Targeting
-exports['qb-target']:AddBoxZone("BlackoutBoxZone",vector3(713.01, 161.07, 81.10), 1.2, 1, { -- The name has to be unique, the coords a vector3 as shown, the 1.5 is the length of the boxzone and the 1.6 is the width of the boxzone, the length and width have to be float values
-  name = "BlackoutBoxZone", -- This is the name of the zone recognized by PolyZone, this has to be unique so it doesn't mess up with other zones
-  heading = 331, -- The heading of the boxzone, this has to be a float value
-  debugPoly = false, -- This is for enabling/disabling the drawing of the box, it accepts only a boolean value (true or false), when true it will draw the polyzone in green
-  minZ = 80.5, -- This is the bottom of the boxzone, this can be different from the Z value in the coords, this has to be a float value
-  maxZ = 81.9, -- This is the top of the boxzone, this can be different from the Z value in the coords, this has to be a float value
-}, {
-  options = { -- This is your options table, in this table all the options will be specified for the target to accept
-    { -- This is the first table with options, you can make as many options inside the options table as you want
-      num = 1, -- This is the position number of your option in the list of options in the qb-target context menu (OPTIONAL)
-      type = "server", -- This specifies the type of event the target has to trigger on click, this can be "client", "server", "command" or "qbcommand", this is OPTIONAL and will only work if the event is also specified
-      event = "ss-blackout:blackout", -- This is the event it will trigger on click, this can be a client event, server event, command or qbcore registered command, NOTICE: Normal command can't have arguments passed through, QBCore registered ones can have arguments passed through
-      icon = 'fa-solid fa-power-off', -- This is the icon that will display next to this trigger option
-      label = 'Turn City Power Off', -- This is the label of this option which you would be able to click on to trigger everything, this has to be a string
-    }
-  },
-  distance = 3.0, -- This is the distance for you to be at for the target to turn blue, this is in GTA units and has to be a float value
-})
+local function AddBlackoutTarget()
+    if GetResourceState('ox_target') == 'started' then
+        exports.ox_target:addBoxZone({
+            name = 'BlackoutBoxZone',
+            coords = vec3(713.01, 161.07, 81.10),
+            size = vec3(1.2, 1.0, 1.4),
+            rotation = 331,
+            debug = false,
+            options = {
+                {
+                    name = 'blackout_power',
+                    icon = 'fa-solid fa-power-off',
+                    label = 'Turn City Power Off',
+                    distance = 3.0,
+                    onSelect = function()
+                        TriggerServerEvent('ss-blackout:blackout')
+                    end
+                }
+            }
+        })
+        print('[ss-blackout] ox_target detected and loaded')
+    elseif GetResourceState('qb-target') == 'started' then
+        exports['qb-target']:AddBoxZone(
+            'BlackoutBoxZone',
+            vector3(713.01, 161.07, 81.10),
+            1.2,
+            1.0,
+            {
+                name = 'BlackoutBoxZone',
+                heading = 331,
+                debugPoly = false,
+                minZ = 80.5,
+                maxZ = 81.9
+            },
+            {
+                options = {
+                    {
+                        type = 'server',
+                        event = 'ss-blackout:blackout',
+                        icon = 'fa-solid fa-power-off',
+                        label = 'Turn City Power Off'
+                    }
+                },
+                distance = 3.0
+            }
+        )
+        print('[ss-blackout] qb-target detected and loaded')
+    else
+        print('[ss-blackout] WARNING: No supported target resource found (qb-target / ox_target)')
+    end
+end 
 
+CreateThread(function()
+    Wait(1000)
+    AddBlackoutTarget()
+end)
